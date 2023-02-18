ceb84a5cd4fa51432abf51be97a42c45bd40b237
Luca <l.cosuti@gmail.com>
2023-02-18 18:57:18 +0100

    Update botClassification.py

--- botClassification.py
+++ botClassification.py
@@ -4,8 +4,6 @@ import seaborn as sns
 
 sns.set_theme(style="white", font_scale=1.0)
 
-
-# read the json file
 with open('file.json') as f:
     data = json.load(f)
 
@@ -35,7 +33,6 @@ for bot in data:
     else:
         other_bots += 1
 
-# plot the pie chart of the categories
 final_data = [financial_bots, fake_followers_bots, spammers_bots,
               self_declared_bots, other_bots, astroturf_bots]
 
@@ -44,19 +41,11 @@ fig = plt.figure(figsize=(10, 7))
 percentages = [financial_bots/len(data)*100, fake_followers_bots/len(data)*100, spammers_bots/len(
     data)*100, self_declared_bots/len(data)*100, other_bots/len(data)*100, astroturf_bots/len(data)*100]
 
-# plot the pie chart without showing the percentages
-
 plt.pie(final_data, startangle=90, colors=[
         'orange', 'lightblue', 'green', 'red', 'purple', 'yellow'], )
 
-# plt.legend(loc="best", bbox_to_anchor=(1.2, 1), labels=['%s: %1.2f %%' % (
-#    l, s) for l, s in zip(["Financial", "Fake Followers", "Spammers", "Self Declared", "Astroturf", "Other"], percentages)])
-
-
-# add the legend with a bigger font size
 plt.legend(loc="best", bbox_to_anchor=(1.2, 1), labels=['%s: %1.2f %%' % (
     l, s) for l, s in zip(["Financial", "Fake Followers", "Spammers", "Self Declared", "Astroturf", "Other"], percentages)], prop={'size': 23})
-
 
 plt.show()
 
